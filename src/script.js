6625a0f92c461d3cef8884d4c4f2819389796b16
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-30 11:13:16 +0200

    Resolved incorrect paths issue

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import { drawGrid } from "../gameFunctions";
+import { drawGrid } from "./gameFunctions.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
