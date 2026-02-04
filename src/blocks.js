921743361c65b12f1a49d9ba6e9048b606149849
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-04 10:28:27 +0200

    File to hold the block templates

---
+++ src/blocks.js
@@ -0,0 +1,23 @@
+export const BLOCK_SHAPES = [
+  // single block
+  [[1]],
+
+  // 2 block line
+  [[1, 1]],
+
+  // 3 block line
+  [[1, 1, 1]],
+
+  // square
+  [
+    [1, 1],
+    [1, 1],
+  ],
+
+  // L shape
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+];
