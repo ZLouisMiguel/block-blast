157fb5436f023a7423c6144871b183b796ae6b72
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-04 10:30:01 +0200

    Testing if blocks render properly

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-import { drawGrid } from "./gameFunctions.js";
+import { drawGrid, drawBlock } from "./gameFunctions.js";
+import { BLOCK_SHAPES } from "./blocks.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -9,24 +10,9 @@ const CELL_SIZE = 60;
 let GAME_GRID = Array.from({ length: GRID_SIZE }, () =>
   Array(GRID_SIZE).fill(0),
 );
-
-/* [
-   Row 0: [0,1,2,3,4,5,6,7,8,9],
-   Row 1: [0,1,2,3,4,5,6,7,8,9],
-   Row 2: [0,1,2,3,4,5,6,7,8,9],
-   Row 3: [0,1,2,3,4,5,6,7,8,9],
-   Row 4: [0,1,2,3,4,5,6,7,8,9],
-   Row 5: [0,1,2,3,4,5,6,7,8,9],
-   Row 6: [0,1,2,3,4,5,6,7,8,9],
-   Row 7: [0,1,2,3,4,5,6,7,8,9],
-   Row 8: [0,1,2,3,4,5,6,7,8,9],
-   Row 9: [0,1,2,3,4,5,6,7,8,9],
-] */
-
-for( let i = 0 ; i < 10 ; i++) GAME_GRID[i][i] = 1;
-for(let i = 9; i >= 0 ; i--) GAME_GRID[9-i][i] =1;
-
-
-
 console.log(GAME_GRID);
 drawGrid(ctx, canvas, GRID_SIZE, CELL_SIZE, GAME_GRID);
+
+const testBlock = BLOCK_SHAPES[4];
+drawBlock(ctx, testBlock, 0, 0, CELL_SIZE);
+drawBlock(ctx, BLOCK_SHAPES[2], canvas.width - (4 * CELL_SIZE), canvas.height - CELL_SIZE, CELL_SIZE);
