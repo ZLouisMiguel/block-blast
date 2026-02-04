8cfd03f7db6b8363407e398dac7e2c0d4c46c27b
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-02-04 10:53:25 +0200

    Block drawing testing and testing for logical placement of blocks from their width and height

--- src/script.js
+++ src/script.js
@@ -15,4 +15,15 @@ drawGrid(ctx, canvas, GRID_SIZE, CELL_SIZE, GAME_GRID);
 
 const testBlock = BLOCK_SHAPES[4];
 drawBlock(ctx, testBlock, 0, 0, CELL_SIZE);
-drawBlock(ctx, BLOCK_SHAPES[2], canvas.width - (4 * CELL_SIZE), canvas.height - CELL_SIZE, CELL_SIZE);
+
+const block = BLOCK_SHAPES[Math.floor(Math.random() * BLOCK_SHAPES.length)];
+const blockWidth  = block[0].length * CELL_SIZE;
+const blockHeight = block.length * CELL_SIZE;
+
+drawBlock(
+  ctx,
+  block,
+  canvas.width  - blockWidth  - CELL_SIZE,
+  canvas.height - blockHeight - CELL_SIZE,
+  CELL_SIZE
+);
